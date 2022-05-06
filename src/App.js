da17ef912f7fa1d879f4c784a46d1062e5dd1094
Steven Nguyen <steyen@umich.edu>
2022-05-05 23:14:00 -0400

    complete component

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@ import "./styles.css";
 import { DateTime } from "./DateTime";
 
 export default function App({ input }) {
-  if (input) {
+  if (Array.isArray(input)) {
+    /* If the prop is an array, return a list of divs,
+    each containing one element of the array. */
     return (
-      <div className="App">
-        <h1>Hi</h1>
-      </div>
+      <ul>
+        {input.map((element) => (
+          <li>
+            <div>{element}</div>
+          </li>
+        ))}
+      </ul>
     );
+  } else if (input) {
+    /* If the prop is anything else,
+    return the value of the prop in a div */
+    return <div>{input}</div>;
   } else {
+    /* If the prop is a falsy value,
+    return a live-updating date and time
+    (update every second) in a div. */
     return (
-      <div className="App">
+      <div>
         <DateTime />
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -5,9 +5,12 @@ import App from "./App";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
+let array = [1, 2, 3, 4, 5, 6, 7];
+let string = "Steven Nguyen";
+let boolean = false;
 
 root.render(
   <StrictMode>
-    <App input="" />
+    <App />
   </StrictMode>
 );
